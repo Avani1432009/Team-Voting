a1e57ac794ab114c79219a841855e71d193e843c
Avani1432009 <85622838+Avani1432009@users.noreply.github.com>
2021-09-25 18:44:17 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -2,7 +2,12 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+    apiKey: "AIzaSyDqFn8Tf2JkjPfcpx6qadLCkWY90HZaSIg",
+    authDomain: "team-voting-42128.firebaseapp.com",
+    projectId: "team-voting-42128",
+    storageBucket: "team-voting-42128.appspot.com",
+    messagingSenderId: "1036044447779",
+    appId: "1:1036044447779:web:c69304670c52d332c5c196"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
